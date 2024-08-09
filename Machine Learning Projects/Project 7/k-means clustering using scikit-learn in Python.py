d8f6ce396a57d99eddc134635e97a6a1fb83f6f0
Aditya Pathak <adityapathak034@gmail.com>
2024-08-09 18:55:05 +0530

    Update and rename k-means lustering using scikit-learn in Python.py to k-means clustering using scikit-learn in Python.py

--- Machine Learning Projects/Project 7/k-means clustering using scikit-learn in Python.py
+++ Machine Learning Projects/Project 7/k-means clustering using scikit-learn in Python.py
@@ -1,5 +1,5 @@
 '''
-We will learn how to perform k-means lustering using scikit-learn in Python.
+We will learn how to perform k-means clustering using scikit-learn in Python.
  We will use cluster analysis to generate a big picture model of the weather at a local station using a
 minute-graunlarity data. In this dataset, we have in the order of millions records. How do we create
 12 clusters our of them?
